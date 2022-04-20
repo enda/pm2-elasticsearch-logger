e4d3770159721774280a9d79452dc2fd7f7c3eba
Jerome Musialak <jerome.musialak@gmail.com>
2022-04-20 17:44:49 +0200

    allow elastic cluster

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # pm2-elasticsearch-logger
 
-[![npm version](https://badge.fury.io/js/pm2-elasticsearch-logger.svg)](https://badge.fury.io/js/pm2-elasticsearch-logger) 
+[![npm version](https://badge.fury.io/js/pm2-elasticsearch-logger.svg)](https://badge.fury.io/js/pm2-elasticsearch-logger)
 [![dependencies Status](https://david-dm.org/hobbyquaker/pm2-elasticsearch-logger/status.svg)](https://david-dm.org/hobbyquaker/pm2-elasticsearch-logger)
 [![Build Status](https://travis-ci.org/hobbyquaker/pm2-elasticsearch-logger.svg?branch=master)](https://travis-ci.org/hobbyquaker/pm2-elasticsearch-logger)
 [![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/sindresorhus/xo)
@@ -27,7 +27,6 @@ PM2 will automatically restart the module after changing an option.
 | option |           | default |
 | ------ | --------- | ------- |
 | elasticUrl | URL of Elasticsearch API | `http://localhost:9200` |
-| insecure | allow https connections to servers with invalid certificate | `false` |
 | index | Elasticsearch index to use | `pm2` |
 | type | Document type to use | `pm2` |
 | host | `host` attribute of the document | `os.hostname()` |
--- index.js
+++ index.js
@@ -1,75 +1,70 @@
 const os = require('os');
 const pmx = require('pmx');
 const pm2 = require('pm2');
-const request =	require('request');
+const elasticsearch = require('elasticsearch');
 
 pmx.initModule({}, (err, conf) => {
-    if (err) {
-        console.error('error on initalizing module', err.message);
-    }
+  if (err) {
+    console.error('error on initalizing module', err.message);
+  }
 
-    const config = {
-        index: conf.index || 'pm2',
-        type: conf.type || 'pm2',
-        host: conf.host || os.hostname(),
-        elasticUrl: conf.elasticUrl || 'http://localhost:9200',
-        insecure: conf.insecure || false
+  const config = {
+    index: conf.index || 'pm2',
+    type: conf.type || 'pm2',
+    host: conf.host || os.hostname(),
+    elasticUrl: conf.elasticUrl || 'http://localhost:9200',
+  };
+
+  const client = new elasticsearch.Client({
+    host: config.elasticUrl.split(','),
+  });
+
+  let url;
+  let currentDate;
+  let index;
+
+  function log(source, msg) {
+    const d = new Date();
+
+    const data = {
+      '@timestamp': d.toISOString(),
+      host: config.host,
+      source,
+      id: msg.process.pm_id,
+      process: msg.process.name,
+      message: msg.data
     };
 
-    let url;
-    let currentDate;
+    const body = JSON.stringify(data);
 
-    function log(source, msg) {
-        const d = new Date();
-
-        const data = {
-            '@timestamp': d.toISOString(),
-            host: config.host,
-            source,
-            id: msg.process.pm_id,
-            process: msg.process.name,
-            message: msg.data
-        };
-
-        const body = JSON.stringify(data);
-
-        const date = d.getDate();
-        if (date !== currentDate) {
-            const index = config.index + '-' + d.getFullYear() + '.' + ('0' + (d.getMonth() + 1)).substr(-2) + '.' + ('0' + date).substr(-2);
-            console.log('sending logs to', config.elasticUrl + '/' + index + '/' + config.type);
-            url = config.elasticUrl + '/' + index + '/' + config.type + '/';
-            currentDate = date;
-        }
-
-        request.post({
-            url,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body,
-            strictSSL: !config.insecure
-        }, err => {
-            if (err) {
-                console.error('error on sending log message', err.message);
-            }
-        });
+    const date = d.getDate();
+    if (date !== currentDate) {
+      index = `${config.index}-${d.getFullYear()}.${('0' + (d.getMonth() + 1)).substr(-2)}.${('0' + date).substr(-2)}`;
+      currentDate = date;
     }
 
-    pm2.launchBus((err, bus) => {
-        if (err) {
-            console.error('error on launching pm2 bus', err.message);
-        }
-
-        bus.on('log:err', data => {
-            if (data.process.name !== 'pm2-elasticsearch-logger') {
-                log('stderr', data);
-            }
-        });
-
-        bus.on('log:out', data => {
-            if (data.process.name !== 'pm2-elasticsearch-logger') {
-                log('stdout', data);
-            }
-        });
+    client.index({
+      index: index,
+      type: config.type,
+      body,
     });
+  }
+
+  pm2.launchBus((err, bus) => {
+    if (err) {
+      console.error('error on launching pm2 bus', err.message);
+    }
+
+    bus.on('log:err', (data) => {
+      if (data.process.name !== 'pm2-elasticsearch-logger') {
+        log('stderr', data);
+      }
+    });
+
+    bus.on('log:out', (data) => {
+      if (data.process.name !== 'pm2-elasticsearch-logger') {
+        log('stdout', data);
+      }
+    });
+  });
 });
